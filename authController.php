e11b3992bddafc146757be75ad5d2f952489e330
eestrada_21 <159752169+ErikTona@users.noreply.github.com>
2024-10-22 20:23:05 -0700

    Add files via upload

---
+++ authController.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+
+if(isset($_POST ['action'];{
+    $correo =  $_POST['correo'];
+    $contraseña = $_POST['contraseña'];
+     switch ($_POST ['action']) {
+        case 'login':
+            $_POST  ['correo', 'contraseña'] = 'login';
+            break;
+        }
+    }))
+     
+ 
+
+
+ class authController{
+    public function access($correo , $contraseña){
+        $correo  = $_POST ['correo'];
+        $contraseña   = $_POST ['contraseña'];
+    }
+
+
+    echo "hola"
+
+    $response=curl_exec($curl)
+    curl_close($curl);
+
+    $response=json_decode($response)
+("location = home.html")
+}
